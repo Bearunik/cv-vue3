b2f729bbff1520db7fbaaa84c59058c3e0727767
Mickael THERAUD <mickael.theraud@ama.bzh>
2022-06-21 17:47:27 +0200

    review display and add theme

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-app>
-    <Header />
-    <v-main>
-      <router-view />
-    </v-main>
-    <Footer />
+  <v-app class="pa-4">
+    <main class="app-container rounded-xl">
+      <Header class="app-header rounded-t-xl" />
+      <v-content class="app-content">
+        <router-view />
+      </v-content>
+      <Footer class="rounded-b-xl" />
+    </main>
   </v-app>
 </template>
 
@@ -21,3 +23,22 @@ import Header from '@/components/Header.vue'
 })
 export default class App extends Vue {}
 </script>
+
+<style scoped>
+.v-application {
+  background: lightgray;
+}
+main {
+  background: white;
+}
+.app-header {
+  padding-bottom: 100px;
+  position: absolute;
+  width: 100%;
+}
+.app-content {
+  position: relative;
+  margin-top: 75px;
+  display: block;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,23 @@
 <template>
-  <v-app-bar color="primary" density="compact">
-    <v-toolbar-title>MITH CV</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <router-link to="/" class="ml-3 mr-3">
-      {{ $t('home') }}
-    </router-link>
-    <router-link to="/about" class="ml-3 mr-3">
-      {{ $t('about') }}
-    </router-link>
-    <v-avatar class="ml-3">
-      <v-img
-        src="https://picsum.photos/512/128?image=660"
-        alt="Bearunik"
-        :aspect-ratio="1"
-        cover
-      ></v-img>
-    </v-avatar>
-  </v-app-bar>
+  <v-card color="primary" class="rounded-b-0 elevation-0">
+    <div class="d-flex px-6 py-4">
+      <div class="pa-2 mr-auto">MITH CV</div>
+      <router-link to="/" class="ml-3 pa-2" outlined tile>
+        {{ $t('home') }}
+      </router-link>
+      <router-link to="/about" class="ml-3 pa-2" outlined tile>
+        {{ $t('about') }}
+      </router-link>
+      <v-avatar class="ml-3 elevation-5" outlined tile>
+        <v-img
+          src="https://picsum.photos/512/128?image=660"
+          alt="Bearunik"
+          :aspect-ratio="1"
+          cover
+        ></v-img>
+      </v-avatar>
+    </div>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -35,6 +36,5 @@ a .v-btn {
 }
 a.router-link-exact-active {
   text-decoration: underline;
-  color: white;
 }
 </style>
