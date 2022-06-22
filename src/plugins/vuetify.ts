4109c18ee981f9066ae89711ae43960ab5041c69
Mickael THERAUD <mickael.theraud@ama.bzh>
2022-06-22 18:19:24 +0200

    review theme color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -10,14 +10,15 @@ const myTheme = {
   colors: {
     background: '#FFFFFF',
     surface: '#FFFFFF',
-    primary: '#159263',
+    primary: '#00bcd4',
     'primary-darken-1': '#3700B3',
-    secondary: '#03DAC6',
+    secondary: '#cddc39',
     'secondary-darken-1': '#018786',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00'
+    accent: '#3f51b5',
+    error: '#f44336',
+    warning: '#ffc107',
+    info: '#2196f3',
+    success: '#4caf50'
   }
 }
 
